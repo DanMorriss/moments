f93f338f4660014c8dd24ce59bd93b31505d7fe6
Dan Morriss <danielmorriss1@gmail.com>
2024-01-17 13:19:29 +0000

    Fix axiosDefaults.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.baseURL = "https://dan-morriss-drf-api-c0c866a91fa3.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
