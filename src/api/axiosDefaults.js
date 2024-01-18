34ae63747fcb28261f13558b24c52bffc3f6f500
DanMorriss <>
2024-01-18 15:21:55 +0000

    Add code for linking codeanywhere for testing in axiosDefaults

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://dan-morriss-drf-api-c0c866a91fa3.herokuapp.com/";
+// axios.defaults.baseURL = "https://8000-danmorriss-drf-api-436c6w6f81.us2.codeanyapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
