a26772209529dcefb0d45095bfa25990122d2ad0
Dan Morriss <danielmorriss1@gmail.com>
2024-01-18 16:51:20 +0000

    Add code to allow user to stay logged in

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = "https://dan-morriss-drf-api-c0c866a91fa3.herokuapp.com/";
-axios.defaults.baseURL = "https://dan-morriss-drf-api-2-fe887fed1f9d.herokuapp.com/";
+axios.defaults.baseURL =
+  "https://dan-morriss-drf-api-2-fe887fed1f9d.herokuapp.com/";
 // axios.defaults.baseURL = "https://8000-danmorriss-drf-api-436c6w6f81.us2.codeanyapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
